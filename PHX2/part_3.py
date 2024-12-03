86e277b0909552b2bd02d759e31e386f3d14db79
HiHi3690 <duimstrajakson@gmail.com>
2024-12-03 15:00:17 -0500

    initial commit for PHX p1, 3, 4

---
+++ PHX2/part_3.py
@@ -0,0 +1,14 @@
+"""
+Part III – Data Management A
+1.Create a master file for the AL (Lg=’AL’) and a master file for the NL
+(Lg=’NL’) by joining the files for the provided 10 years in the ‘Data
+Management’ folder.
+2. Using the master files separately, create the following for both leagues
+(AL and NL) separately:
+    a. Top 10 player/year combinations ranked by WAR (higher is better) for
+    the decade (2007-2016) for each league
+    b. Top 10 player/year combinations (Age > 30) by WAR (higher is better)
+    for the decade (2007-2016) for each league
+    c. Sum of WAR by the entire year (ie, total WAR for 2007), then rank the
+    10 years by WAR (higher is better)
+"""
