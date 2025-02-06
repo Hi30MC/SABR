b5f009344ec04b0c38e9dfec6594cc58ea84dced
HiHi3690 <duimstraash@gmail.com>
2025-02-06 16:02:13 -0500

    bruteforce the model

--- DD/DD1/main.py
+++ DD/DD1/main.py
@@ -1,3 +1,5 @@
+import timeit
+from itertools import chain, combinations
 import pandas as pd
 import statsmodels.api as sm
 
@@ -43,24 +45,40 @@ exact same, mainly bc salary has 0 effect on performance
 
 #Part 3: model train and create
 
+start = timeit.default_timer()
+
 train_data2 = pd.read_excel("DD/DD1/trainp3.xlsx")
 
-cols = "H 1B 2B 3B HR SO SB BA SLG OPS OBA".split(" ")
+cols = "H 1B 2B 3B HR BB SO SB BA SLG OPS OBA".split(" ")
 
-reg3 = sm.OLS(train_data2["W"], train_data2[cols])
-model3 = reg3.fit()
+def powerset(iterable):
+    s = list(iterable)
+    return sum([[list(x) for x in combinations(s, r)] for r in range(1,len(s)+1)], [])
 
-# print(model3.pvalues)
-# print(model3.params)
-# print(model3.summary())
+outs = powerset(cols)
+# print(len(outs)) #4095
 
 test_data = pd.read_excel("DD/DD1/test.xlsx")
+test_check = test_data["W"]
+gen = ""
+min_MAE = 10000
+for vars in outs:
+    # modtime = timeit.default_timer()
+    reg3 = sm.OLS(train_data2["W"], train_data2[vars])
+    model3 = reg3.fit()
 
-with open("DD/DD1/out.csv", "w") as f:
-    data = [*model3.predict(test_data[cols])]
-    f.write("ID,W\n")
-    for i,num in enumerate(data):
-        f.write(f"{i},{num}\n")
+    test_MAE = pd.DataFrame(model3.predict(test_data[vars]), columns=["W"]).sub(pd.DataFrame(test_check, columns=["W"])).abs().mean()
+    min_MAE,gen = min(min_MAE, test_MAE["W"]), vars
+    # print(f"Time taken for {vars}: {timeit.default_timer() - modtime}")
+
+print(min_MAE, gen) # ['H', '1B', '2B', '3B', 'HR', 'BB', 'SO', 'SB', 'BA', 'SLG', 'OPS', 'OBA'] gives best MAE = 7.6022
+print(f"Time taken: {timeit.default_timer() - start}") # takes above code 11.115 seconds to run above code
+
+# generate list of W and write to output file
+with open("DD/DD1/output.txt", "w") as f:
+    f.write("W\n")
+    for i in model3.predict(test_data[gen]):
+        f.write(f"{i}\n")
 """
 P values are under .05
 
@@ -87,14 +105,14 @@ bases should be pos, but 3b and hr are neg, same with hits, etc
 
 #4: model other sport
 
-train_data3 = pd.read_excel("DD/DD1/trainp4.xlsx")
+# train_data3 = pd.read_excel("DD/DD1/trainp4.xlsx")
 
-reg4 = sm.OLS(train_data3["W"], train_data3[["PS", "PA"]])
-model4 = reg4.fit()
+# reg4 = sm.OLS(train_data3["W"], train_data3[["PS", "PA"]])
+# model4 = reg4.fit()
 
-print(model4.pvalues)
-print(model4.params)
-print(model4.summary())
+# print(model4.pvalues)
+# print(model4.params)
+# print(model4.summary())
 
 
 """
